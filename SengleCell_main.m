47baff9c94438feeb6f2b0258d07c61862a766e7
maxwellsdm <maxwellsdm1867@gmail.com>
2020-03-05 13:05:47 +0800

    1

---
+++ SengleCell_main.m
@@ -0,0 +1,84 @@
+close all
+clear all
+clc
+%main process
+hmmg=[];
+oug=[];
+load('D:\RiekeLab\cellattach\mega2.mat')
+%to check how many g values are there in the Hmm and OU
+for i=1:length(mega)
+    temp = mega{1,i};
+    es =length(temp);
+    for j=1:es%add in noise step data later
+        if strcmp(temp(j).meta.displayName,'HMM Single')
+            hmmg =[hmmg temp(j).meta.correlationTime];
+        elseif strcmp(temp(j).meta.displayName,'OU Single')
+            oug =[oug temp(j).meta.correlationTime];
+        end
+    end
+end
+hmmg_u = unique(hmmg);
+oug_u = unique(oug);
+
+%% cell type identification(human eye for now)
+close all
+type = 99*ones(1,size(mega,2));
+for i=1:length(mega)
+    temp = mega{1,i};
+    es =length(temp);
+    figure;
+    hold on
+    for j=1:es
+        if strcmp(temp(j).meta.displayName,'Led Pulse')
+            plot(temp(j).epoch)
+        end
+    end
+    hold off
+    type(i) = input('cell type?')
+    %on ststain = 1
+    %on transiant = 2
+    %off sustain =3
+    %off transiant =4
+    %unknown =5
+end
+cabinet.type=type;
+%% make them into a sorted megafile
+cabinet.hmm=cell(2,length(hmmg_u));
+for k = 1:length(hmmg_u)
+    target = hmmg_u(k);
+    cabinet.hmm{2,k} = target;
+    for i=1:length(mega)%cell id 
+        temp = mega{1,i};
+        es =length(temp);
+        for j =1:es%all the epochs
+            if strcmp(temp(j).meta.displayName,'HMM Single') & temp(j).meta.correlationTime==target
+                temp(j).id=i;
+                cabinet.hmm{1,k} =  [cabinet.hmm{1,k}; temp(j)];
+            end
+        end
+    end
+end
+
+cabinet.ou=cell(2,length(oug_u));
+
+for k = 1:length(oug_u)
+    target = oug_u(k);
+    cabinet.ou{2,k} = target;
+    for i=1:length(mega)%cell id 
+        temp = mega{1,i};
+        es =length(temp);
+        for j =1:es%all the epochs
+            if strcmp(temp(j).meta.displayName,'OU Single') & temp(j).meta.correlationTime==target
+                temp(j).id=i;
+                cabinet.ou{1,k} =  [cabinet.ou{1,k}; temp(j)];
+            end
+        end
+    end
+end
+
+
+
+
+
+
+
---
+++ SingleCellInfos.m
@@ -0,0 +1,56 @@
+%this is where we do stats
+tic
+raw_sorted = cabinet.hmm;
+results = cell(2,size(cabinet.hmm,2));%spike and stimulus side by side
+results(2,:)=raw_sorted(2,:);
+for i = 1:length(raw_sorted)%g values
+    tar = raw_sorted{1,i};
+    for j= 1:length(tar)
+        spikes = spike_detection(tar(j).epoch, 10000);%fix this later
+        stim = HMM_reborn2(tar(j).meta.correlationTime,tar(j).meta.seed, 10000,length(tar(j).epoch)/10000,5);
+        FR = BinSpk1(0.040,spikes,length(tar(j).epoch)/10000);
+        dwn_stim = downsample(stim,400);
+        [MI,MI_shuffled,t] = tsmi_clean(FR(1:end-1),dwn_stim);
+        [MI_height, preloc] = max(MI(161:361));
+        Mi_loc = t(161-1+preloc);
+        temp1{1,i}(j).spikes = spikes;
+        temp1{1,i}(j).ph = MI_height; 
+        temp1{1,i}(j).pl = Mi_loc;
+        temp1{1,i}(j).stim = stim;
+        temp1{1,i}(j).FR = FR;
+        temp1{1,i}(j).MI = MI;
+        temp1{1,i}(j).ID =tar(j).id;
+    end
+    
+end
+results(1,:) = temp1;
+results{3,1}=t;
+toc
+
+tic
+raw_sorted = cabinet.ou;
+results = cell(2,size(cabinet.ou,2));%spike and stimulus side by side
+results(2,:)=raw_sorted(2,:);
+for i = 1:length(raw_sorted)%g values
+    tar = raw_sorted{1,i};
+    for j= 1:length(tar)
+        spikes = spike_detection(tar(j).epoch, 10000);%fix this later
+        stim =  OU_reborn(tar(j).meta.correlationTime,tar(j).meta.seed, 10000,length(tar(j).epoch)/10000);
+        FR = BinSpk1(0.040,spikes,length(tar(j).epoch)/10000);
+        dwn_stim = downsample(stim,400);
+        [MI,MI_shuffled,t] = tsmi_clean(FR(1:end-1),dwn_stim);
+        [MI_height, preloc] = max(MI(161:361));
+        Mi_loc = t(161-1+preloc);
+        temp1{1,i}(j).spikes = spikes;
+        temp1{1,i}(j).ph = MI_height; 
+        temp1{1,i}(j).pl = Mi_loc;
+        temp1{1,i}(j).stim = stim;
+        temp1{1,i}(j).FR = FR;
+        temp1{1,i}(j).MI = MI;
+        temp1{1,i}(j).ID =tar(j).id;
+    end
+    
+end
+results(1,:) = temp1;
+results{3,1}=t;
+toc
