96a603034f0adb418b663f21787112a7db1fc129
ArthurHung <49833088+maxwellsdm1867@users.noreply.github.com>
2020-07-30 17:57:30 +0800

    estimater

--- cellbycell.m
+++ cellbycell.m
@@ -1,39 +1,65 @@
 %this is for generating the figures for one condition for one cell type
-close all 
+close all
 clear all
 clc
 
 tar = 3;
-load('D:\RiekeLab\spikes\191217\hmm_r.mat')
-load('ou_results_p.mat')
-load('D:\RiekeLab\spikes\191217\type.mat')
-
+load('hmm_r.mat')
+load('ou_r.mat')
 %%
+type{1,1}= 'cone biploar';
+type{1,2}= 'cone biploar';
+type{1,3}= 'cone biploar';
+
 close all
 for cell_id = 1:length(type)
-    figure
+    figure('units','normalized','outerposition',[0 0 1 1])
     hold on
     sk = [];
     parm = [];
-    for hmm_i = 1:size(hmm_r,2)
+    for hmm_i = 1:size(hmm_r,2)%get the color scheme
         tempz = hmm_r{1,hmm_i};
-        for k = 1:length(tempz)
+        for k = 1:length(tempz)%one g-value
             if tempz(k).ID == cell_id
-                plot(hmm_r{3,1},tempz(k).MI,'LineWidth',1.5)
                 sk = [sk 0];%0 is hmm
                 parm = [parm hmm_r{2,hmm_i} ];
-                k
             end
         end
     end
+    n_colors = sum(sk==0);
+    colors = distinguishable_colors(n_colors);
+    ci = 0;
+    for hmm_i = 1:size(hmm_r,2)
+        tempz = hmm_r{1,hmm_i};
+        for k = 1:length(tempz)%one g-value
+            if tempz(k).ID == cell_id
+                ci = ci+1;
+                plot(hmm_r{3,1},tempz(k).MI,'LineWidth',1.5, 'Color',colors(ci,:))
+            end
+        end
+    end
+    
     for ou_i =1:size(ou_r,2)
-        tempy = results{1,ou_i};
+        tempy = ou_r{1,ou_i};
         for ck = 1:length(tempy)
             if tempy(ck).ID == cell_id
-                plot(ou_r{3,1},tempy(ck).MI,'--','LineWidth',1.2)
+                %plot(ou_r{3,1},tempy(ck).MI,'--','LineWidth',1.2,'Color',colors(ci,:))
                 sk = [sk 1];%1 is ou
-                parm = [parm results{2,ou_i} ];
-                ck
+                parm = [parm ou_r{2,ou_i} ];
+            end
+        end
+    end
+    
+    n_colors = sum(sk==1);
+    colors = distinguishable_colors(n_colors);
+    ci = 0;
+    
+    for ou_i =1:size(ou_r,2)
+        tempy = ou_r{1,ou_i};
+        for ck = 1:length(tempy)
+            if tempy(ck).ID == cell_id
+                ci = ci+1;
+                plot(ou_r{3,1},tempy(ck).MI,'--','LineWidth',1.2,'Color',colors(ci,:))
             end
         end
     end
@@ -46,11 +72,13 @@ for cell_id = 1:length(type)
         end
     end
     legend(tmpn)
-    vline(0)
+    
     xlim([-2000 2000])
     hold off
     xlabel('time shift(ms)')
     ylabel('bit/sec')
-    title(['cell' num2str(cell_id) 'type' num2str(type(cell_id))])
-    saveas(gcf,['type' num2str(type(cell_id)) 'cell' num2str(cell_id) '.jpg'])
+    title(['cell' num2str(cell_id) ' type ' type{1,cell_id}])
+    xline(0,'-','DisplayName','zero time shift');
+    saveas(gcf,['type' type{1,cell_id} 'cell' num2str(cell_id) '.jpg'])
+    
 end
--- g_estimator.m
+++ g_estimator.m
@@ -1,8 +1,8 @@
-function [t2p, rpl,intensity]=  g_estimator(spike,stim,stim_rate, tar_rate)
+function [t2p, rpl,intensity,temp_min,temp_max]=  g_estimator(spike,stim,stim_rate, tar_rate)
 %the script is used for estimation of the g values using the response to a
 %response of a 200 ms constant
 %quantify (1) time to peak (2)the ratio of peaked and last bin of the
-%step(3)scale with intensity level
+%step(3)scale with intensity level (4)demo epochs(highest and lowest intensity) with error bar
 dwn_stim = downsample(stim,stim_rate/tar_rate);%downsample the stimulus
 fr = BinSpk1(1/tar_rate,spike,length(stim)/stim_rate);%calculate the spike per bin, the bing rate is defined by tar_rate
 step_size = (1/tar_rate); %time duration of the step after downsapling (in seconds)
@@ -15,16 +15,28 @@ intensity = [];%intensity of the steps
 %extract infomation from indivusal steps
 
 idx = 1:bins_per_step:length(dwn_stim);
-for i = 1:(length(dwn_stim)/bins_per_step )%ith state
+cnt = 0;
+for i = 1:(length(dwn_stim)/bins_per_step )%ith step
+    
     temp_fr = fr(idx(i):(idx(i+1)-1));
     temp_stim = dwn_stim(idx(i):(idx(i+1)-1));
     [M,I] = max(temp_fr);
-    
+    temp_min = []; % response to min intensity
+    temp_max = [];% response to max intensity
     if M ~= 0% & temp_fr(end)~=0
         t2p  =[t2p I*step_size*1000];%time to peak in ms
         rpl =[rpl temp_fr(end)/M];
         intensity = [intensity temp_stim(1)];
+        %extract the demo firing rate
+        if intensity = min(dwn_stim)
+            temp_min = [temp_min;temp_fr];
+        elseif intensity = max(dwn_stim)
+            temp_max =  [temp_max;temp_fr];
+        end
+        
     end
+    
+    
 end
 
 end
